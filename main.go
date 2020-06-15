3191c15b4de2021352aefab3edaef57b58079e0e
siangT <aslove86305@gmail.com>
2020-06-15 17:43:20 +0800

    1.0.1

--- main.go
+++ main.go
@@ -6,25 +6,11 @@ func main() {
 	for i := 1; i <= 5; i++ {
 		for j := 1; j <= i; j++ {
 			fmt.Print(i, "2 *")
-<<<<<<< HEAD
 			fmt.Print("1")
 			fmt.Print(i, "44 *")
 			fmt.Print("-")
-<<<<<<< HEAD
-<<<<<<< HEAD
 			fmt.Print("step2")
-=======
 			fmt.Print("step5")
->>>>>>> origin/nplus
-=======
-			fmt.Print("step1")
->>>>>>> origin/aa
-=======
-			fmt.Print("step7")
-=======
-			fmt.Print("step6")
->>>>>>> origin/minus
->>>>>>> origin/plus
 
 		}
 		fmt.Println()
